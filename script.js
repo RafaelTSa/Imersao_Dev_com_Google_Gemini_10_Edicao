55f0ee5539247885255b72a8990a8774271ea688
Rafael T Santos <texera09@gmail.com>
2025-11-20 20:44:39 -0300

    altera console para renderizarCards

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let dados = [];
 async function iniciarBusca() {
     let resposta = await fetch("data.json");
     dados = await resposta.json();
-    console.log(dados);
+    renderizarCards(dados);
 }
