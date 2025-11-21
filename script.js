60f0a4be342fa308492138d34b689c312952a191
Rafael T Santos <texera09@gmail.com>
2025-11-20 21:37:12 -0300

    adiciona card dentro do loop da função

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ async function iniciarBusca() {
 }
 
 function renderizarCards(dados) {
-    for (let dado of dados)
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <h2>JavaScript</h2>
+        <p>1995</p>
+        <p>JavaScript é uma linguagem de programação</p>
+        <a href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript" target="_blank">Saiba mais</a>    
+        `;
+        cardContainer.appendChild(article);
+    }
 }
